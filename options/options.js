caec3ea6059fcc5d9048ce0d09f4353816315ee0
Kurt Madsen <kurt@crowdbotics.com>
2023-05-26 13:00:39 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "ent-demo-vodafone-v-41023",
-  url: "https://ent-demo-vodafone-v-41023.botics.co",
-  api: "https://ent-demo-vodafone-v-41023.botics.co/api/v1"
-}
+  "name": "ent-demo-vodafone-v-41023",
+  "url": "https://ent-demo-vodafone-v-41023.botics.co",
+  "api": "https://ent-demo-vodafone-v-41023.botics.co/api/v1",
+  "initialRoute": "vdfItHome"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
