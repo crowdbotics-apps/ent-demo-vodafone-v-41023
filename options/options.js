08cda6b383bdc5ffdf2245249423285bb7c05646
Crowdbotics <team@crowdbotics.com>
2023-05-26 18:23:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ent-demo-vodafone-v-41023",
+  url: "https://ent-demo-vodafone-v-41023.botics.co",
+  api: "https://ent-demo-vodafone-v-41023.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
